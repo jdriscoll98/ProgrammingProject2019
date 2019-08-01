efb11bd9a4f0f76b8924a0d9722cbeb494e5bddc
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-08-01 17:16:20 -0400

    changed bait defender to attack defender

--- src/edu/ufl/cise/cs1/controllers/StudentDefenderController.java
+++ src/edu/ufl/cise/cs1/controllers/StudentDefenderController.java
@@ -16,24 +16,24 @@ public final class StudentDefenderController implements DefenderController {
 
 	public int[] update(Game game, long timeDue) {
 
-		//There will be 4 defenders: Chase, Ambush, Patrol, and Gaurd
+		//There will be 4 defenders: Chase, Ambush, Patrol, and Guard
 		// Smart: Chases until attacker gets close to a pill then flees
 		// Chase: Chases after attacker unless vulnerable
 		// Bait: Smart Defender, but gets close to attacker when hes waiting to eat pill
-		// Gaurd: Chase until 2 Power Pills left, then "gaurd" the furthest power pill from the attacker
+		// Guard: Chase until 2 Power Pills left, then "Guard" the furthest power pill from the attacker
 		// Since the defenders move at the same speed as the attacker, it is important that they all have different strategies in order to trap the attacker
 		Attacker attacker = game.getAttacker();
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
-		actions[0] = getSmartDirection(game, enemies.get(0), attacker, enemies.get(1).getPossibleDirs());
-		actions[1] = getGaurdDirection(game, enemies.get(1), attacker, enemies.get(1).getPossibleDirs());
-		actions[2] = getChaseDirection(game, enemies.get(2), attacker, enemies.get(2).getPossibleDirs());
-		actions[3] = getBaitDirection(game, enemies.get(3), attacker, enemies.get(3).getPossibleDirs());
+		actions[0] = getAttackDefender(game, enemies.get(0), attacker, enemies.get(0).getPossibleDirs());
+		actions[1] = getSmartDirection(game, enemies.get(1), attacker, enemies.get(1).getPossibleDirs());
+		actions[2] = getGuardDirection(game, enemies.get(2), attacker, enemies.get(2).getPossibleDirs());
+		actions[3] = getChaseDirection(game, enemies.get(3), attacker, enemies.get(3).getPossibleDirs());
 
 
 		return actions;
 	}
-	public int getGaurdDirection(Game game, Defender defender, Attacker attacker, List<Integer> possibleDirs){
+	public int getGuardDirection(Game game, Defender defender, Attacker attacker, List<Integer> possibleDirs){
 		List<Node> powerPillList = game.getPowerPillList();
 		List<Node> powerPillsLeft = new ArrayList<>();
 		for (Node node: powerPillList) {
@@ -54,7 +54,7 @@ public final class StudentDefenderController implements DefenderController {
 		Node location = attacker.getLocation();
 		int direction = defender.getNextDir(location, true);
 		if (defender.isVulnerable()) {
-			return -direction;
+			return defender.getNextDir(location, false);
 		}
 		return direction;
 	}
@@ -74,20 +74,35 @@ public final class StudentDefenderController implements DefenderController {
 		}
 		return defender.getNextDir(attackerLocation, true);
 	}
-	public int getBaitDirection(Game game, Defender defender, Attacker attacker, List<Integer> possibleDirs) {
+	public int getAttackDefender(Game game, Defender defender, Attacker attacker, List<Integer> possibleDirs) {
 		Node attackerLocation = attacker.getLocation();
-		List<Node> powerPillList = game.getPowerPillList();
-		int max_distance = 50074;
-		for (Node node: powerPillList) {
-			if (node != null) {
-				if (attackerLocation.getPathDistance(node) < max_distance) {
-					max_distance = attackerLocation.getPathDistance(node);
+		List<Defender> defenders = game.getDefenders();
+		List<Defender> vulnerableDefenders = new ArrayList<>();
+		int closestPathDistance = 50074;
+		if (!defender.isVulnerable()) {
+			for (Defender _defender : defenders) {
+				if (_defender.isVulnerable()) {
+					vulnerableDefenders.add(_defender);
 				}
 			}
+			if (vulnerableDefenders.size() > 0) {
+				Defender closestDefender = defender;
+				for (Defender _defender : vulnerableDefenders) {
+					int pathToDefender = attackerLocation.getPathDistance(_defender.getLocation());
+					if (attackerLocation.getPathDistance(_defender.getLocation()) < closestPathDistance) {
+						closestPathDistance = pathToDefender;
+						closestDefender = _defender;
+					}
+				}
+				return defender.getNextDir(closestDefender.getLocation(), true);
+
+			} else {
+				return getChaseDirection(game, defender, attacker, possibleDirs);
+			}
 		}
-		if (max_distance < 25 && max_distance > 5) {
-			return defender.getNextDir(attackerLocation, false);
+		else {
+			return getChaseDirection(game, defender, attacker, possibleDirs);
 		}
-		return defender.getNextDir(attackerLocation, true);
+
 	}
 }
